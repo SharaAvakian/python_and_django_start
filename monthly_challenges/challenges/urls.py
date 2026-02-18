28ad583fe64d0ed5954ef1b9a9d99d520e77cf4b
SharaAvakian <Sharaavakian2000@gmail.com>
2026-02-18 20:02:17 +0400

    004

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:month>', views.monthly_challenges_by_number,name="month_"),
-    path('<str:month>', views.monthly_challenge),
+    path('<int:month>', views.monthly_challenges_by_number),
+    path('<str:month>', views.monthly_challenge,name="month_"),
 ]
 
