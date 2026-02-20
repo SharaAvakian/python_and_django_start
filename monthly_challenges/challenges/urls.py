523b8d5134cb6669d79ef383ec58864f77a67b95
SharaAvakian <164620909+SharaAvakian@users.noreply.github.com>
2026-02-20 19:15:06 +0400

    day 4 -0.02

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.challenges),
+    path("",views.challenges, name="index"),
     path('<int:month>/', views.monthly_challenges_by_number),
     path('<str:month>/', views.monthly_challenge,name="month_"),
 
