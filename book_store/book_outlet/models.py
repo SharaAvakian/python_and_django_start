a01173c77e6d71b9f41a46596bc973339d0ff652
SharaAvakian <Sharaavakian2000@gmail.com>
2026-02-25 05:04:05 +0400

    models query
    After creating new columns in the class ,using pytohn shell i created mutiple entries of book and also learned baout null and defult validations , also validations fo rintegers, after that i used different way to fetch data such as .get() and .FIlter() and later for more comlex rules imported django.db.models Q and used the Q function for filterin with | to us eit as "either "

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator,MaxValueValidator
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    rating = models.DecimalField(decimal_places=2, max_digits=5)
+    rating = models.DecimalField(decimal_places=2, max_digits=5, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    author = models.CharField(null=True , max_length=100)
+    is_bestseller = models.BooleanField( default=False)
 
+    def __str__(self):
+        return f'{self.title} , Rating: {self.rating}'
 
