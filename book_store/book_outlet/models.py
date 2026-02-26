bc2df69ca1794cddd33a759275957cc1de79a002
SharaAvakian <Sharaavakian2000@gmail.com>
2026-02-26 13:50:59 +0400

    day 7
    craeting models and importing them in the views section to later interpolate in the html files eaily , in he models section i also learned howo to modify the save() method and any othe rmethod really with creating a function with the method being oversiden howeverway i want to like adding args and kwargs to save method to add "slug" aka url friendly workding with reverse method and usign same name method as callign urls and thus in the shel just by saving the old entried new data was added in the DB as slug field

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=100)
     rating = models.DecimalField(decimal_places=2, max_digits=5, validators=[MinValueValidator(0), MaxValueValidator(5)])
     author = models.CharField(null=True , max_length=100)
     is_bestseller = models.BooleanField( default=False)
-
+    slug = models.SlugField(default='',null=False,db_index=True)
+    def get_absolute_url(self):
+        return reverse('book_detail',args=[self.slug])
+    def save(self, *args, **kwargs):
+        self.slug= slugify(self.title)
+        super().save(*args, **kwargs)
     def __str__(self):
         return f'{self.title} , Rating: {self.rating}'
 
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.db.models.aggregates import Avg
+from django.shortcuts import render,get_object_or_404
+from .models import Book
 
 # Create your views here.
+def index(request):
+    books = Book.objects.all().order_by("-id").reverse()
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg('rating'))['rating__avg']
+    return render(request,"book_outlet/index.html",{"books":books, "total_books":num_books,"average_rating":avg_rating})
+
+def book_detail(request,slug):
+    book = get_object_or_404(Book,slug=slug)
+    return render(request,"book_outlet/book_detail.html",{"title":book.title  , "author":book.author, "rating":book.rating, "is_bestseller":book.is_bestseller})
