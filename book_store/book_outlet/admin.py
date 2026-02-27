10608fe8c3f398a988d9c171f6fdda861aef43dd
SharaAvakian <Sharaavakian2000@gmail.com>
2026-02-28 00:47:08 +0400

    day 8 0.001
    models in admin panel pretty cool

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Book
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('author','rating')
+    search_fields = ('title',)
+    list_display = ('title','author','rating')
+admin.site.register(Book,BookAdmin)
