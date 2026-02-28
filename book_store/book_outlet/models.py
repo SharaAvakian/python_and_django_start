ccfe5ed2063ac87411666fa2e8333a426166c01f
SharaAvakian <Sharaavakian2000@gmail.com>
2026-02-28 21:00:57 +0400

    still on day 8 0.001
    learning relationships

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,12 +1,19 @@
+from django.contrib import admin
 from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     rating = models.DecimalField(decimal_places=2, max_digits=5, validators=[MinValueValidator(0), MaxValueValidator(5)])
-    author = models.CharField(null=True , max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     is_bestseller = models.BooleanField( default=False)
     slug = models.SlugField(default='',null=False,db_index=True)
     def get_absolute_url(self):
