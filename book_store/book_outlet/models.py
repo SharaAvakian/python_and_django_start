43556af9f7092b85afdb994077e15d4bb4f47a04
SharaAvakian <Sharaavakian2000@gmail.com>
2026-03-05 01:50:59 +0400

    models one-to-many

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -6,14 +6,18 @@ from django.utils.text import slugify
 # Create your models here.
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,default='')
+    last_name = models.CharField(max_length=100,default='')
+    def fulbabyl_name(self):
+        return f'Name: {self.name} , L_name: {self.last_name}'
+    def __str__(self):
+        return self.full_name()
 
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     rating = models.DecimalField(decimal_places=2, max_digits=5, validators=[MinValueValidator(0), MaxValueValidator(5)])
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,related_name='books',null=True)
     is_bestseller = models.BooleanField( default=False)
     slug = models.SlugField(default='',null=False,db_index=True)
     def get_absolute_url(self):
